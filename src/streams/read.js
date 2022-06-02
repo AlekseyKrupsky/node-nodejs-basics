492c51e2d0b6c286137932fcd34d20f18c3a7603
Aleksey Krupsky <aleksey.krupsky@wrenkitchens.com>
2022-06-02 15:21:17 +0300

    WEEK 1. Read

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,3 +1,10 @@
+import { createReadStream } from 'node:fs';
+import { stdout } from 'node:process';
+
 export const read = async () => {
-    // Write your code here 
+    const readableStream =  createReadStream('./files/fileToRead.txt');
+
+    readableStream.pipe(stdout);
 };
+
+read();
