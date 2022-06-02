cee655c64a0a9f11db8c7bf09db7a944edadea29
Aleksey Krupsky <aleksey.krupsky@wrenkitchens.com>
2022-06-02 11:25:57 +0300

    Week 1. FS rename

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,3 +1,24 @@
+import { access, constants, rename as renameFile } from 'node:fs';
+
 export const rename = async () => {
-    // Write your code here 
+    const wrongFileNamePath = './files/wrongFilename.txt';
+    const properFileNamePath = './files/properFilename.md';
+
+    access(properFileNamePath, constants.F_OK, (err) => {
+        if (null === err) {
+            throw new Error('FS operation failed');
+        }
+    });
+
+    renameFile(wrongFileNamePath, properFileNamePath, (err) => {
+        if (null !== err && err.code === 'ENOENT') {
+            throw new Error('FS operation failed');
+        }
+
+        if (null === err) {
+            console.log('File was renamed');
+        }
+    });
 };
+
+rename();
