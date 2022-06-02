c52f7cdff8f06f7d7a16d9e203ab532cfed72965
Aleksey Krupsky <aleksey.krupsky@wrenkitchens.com>
2022-06-02 10:08:23 +0300

    Week 1. FS create

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,3 +1,21 @@
+import { access, constants, writeFile } from 'node:fs';
+
 export const create = async () => {
-    // Write your code here 
+    const filePath = './files/fresh.txt';
+
+    access(filePath, constants.F_OK, (err) => {
+        if (!err) {
+            throw new Error('FS operation failed');
+        }
+    });
+
+    writeFile(filePath, 'I am fresh and young', (err) => {
+        if (err) {
+            err.message = 'Write operation failed';
+
+            throw err;
+        }
+    });
 };
+
+create();
