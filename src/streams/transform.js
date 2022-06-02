1531098619d2239e110a105cc8e2bf2d397ba6fe
Aleksey Krupsky <aleksey.krupsky@wrenkitchens.com>
2022-06-02 16:07:22 +0300

    WEEK 1. Transform

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,3 +1,14 @@
+import { stdin, stdout } from 'node:process';
+import { Transform } from 'node:stream';
+
 export const transform = async () => {
-    // Write your code here 
+    const reverseText = new Transform({
+        transform(chunk, encoding, callback) {
+            callback(null, chunk.toString().split('').reverse().join(''));
+        }
+    });
+
+    stdin.pipe(reverseText).pipe(stdout);
 };
+
+transform();
