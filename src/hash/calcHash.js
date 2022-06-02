36d3933c47c231ce7d041a128dfba524b30a9cd1
Aleksey Krupsky <aleksey.krupsky@wrenkitchens.com>
2022-06-02 15:00:22 +0300

    WEEK 1. Hash

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,3 +1,17 @@
+import { readFile } from 'node:fs';
+import { createHash } from 'node:crypto';
+
 export const calculateHash = async () => {
-    // Write your code here 
+    readFile('./files/fileToCalculateHashFor.txt', 'utf8', (err, data) => {
+        if (null !== err && err.code === 'ENOENT') {
+            throw new Error('FS operation failed');
+        }
+
+        const hash = createHash('sha256');
+        hash.update(data);
+
+        console.log(`Hex: ${hash.digest('hex')}`);
+    });
 };
+
+calculateHash();
