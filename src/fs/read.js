58347976e9114840b48c7485941edc7ed45ace58
Aleksey Krupsky <aleksey.krupsky@wrenkitchens.com>
2022-06-02 12:01:12 +0300

    Week 1. FS

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,3 +1,13 @@
+import { readFile } from 'node:fs';
+
 export const read = async () => {
-    // Write your code here 
+    readFile('./files/fileToRead.txt', 'utf8', (err, data) => {
+        if (null !== err && err.code === 'ENOENT') {
+            throw new Error('FS operation failed');
+        }
+
+        console.log(data);
+    });
 };
+
+read();
