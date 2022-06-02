6dc8a87dde71329eac1940a21dbeeb4ebd9bec2b
Aleksey Krupsky <aleksey.krupsky@wrenkitchens.com>
2022-06-02 10:51:49 +0300

    Week 1. FS copy

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,3 +1,30 @@
+import { access, constants, cp, opendir } from 'node:fs';
+
 export const copy = async () => {
-    // Write your code here 
+    const sourceDir = './files';
+    const destinationDir = './files_copy';
+
+    opendir(sourceDir, (err) => {
+        if (null !== err) {
+            throw new Error('FS operation failed.');
+        }
+
+        access(destinationDir, constants.F_OK, (err) => {
+            if (null === err) {
+                throw new Error('FS operation failed.');
+            }
+        });
+
+        const options = {
+            recursive: true,
+        };
+
+        cp(sourceDir, destinationDir, options, (err) => {
+            if (null === err) {
+                console.log('Files were copies successful');
+            }
+        });
+    });
 };
+
+copy();
