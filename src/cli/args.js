a1ffe87096d03ddf16e3a9f53e5402ffd1d19948
Aleksey Krupsky <aleksey.krupsky@wrenkitchens.com>
2022-06-02 12:34:17 +0300

    WEEK 1. CLI args

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,3 +1,23 @@
+import { argv } from 'node:process';
+
 export const parseArgs = () => {
-    // Write your code here 
+    const propPrefix = '--';
+
+    let argumentsString = [];
+
+    argv.forEach((value, index) => {
+        if (value.startsWith(propPrefix)) {
+            if (argv[index + 1] !== undefined && !argv[index + 1].startsWith(propPrefix)) {
+                const propName = value.replace(propPrefix, '');
+
+                argumentsString.push(`${propName} is ${argv[index + 1]}`)
+            } else {
+                console.log(`Argument ${value} doesn\'t have value`);
+            }
+        }
+    });
+
+    console.log(argumentsString.join(', '));
 };
+
+parseArgs();
