8054dd25a6baea0a8bce8aa9f8d6ab8a624cfee3
Aleksey Krupsky <aleksey.krupsky@wrenkitchens.com>
2022-06-02 16:31:54 +0300

    WEEK 1. Zlib

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,3 +1,19 @@
+import { createGzip } from 'node:zlib';
+import { pipeline } from 'node:stream';
+import { createReadStream, createWriteStream } from 'node:fs';
+
 export const compress = async () => {
-    // Write your code here 
+    const gzip = createGzip();
+    const source = createReadStream('files/fileToCompress.txt');
+    const destination = createWriteStream('files/archive.gz');
+
+    pipeline(source, gzip, destination, (err) => {
+        if (err) {
+            throw new Error('Error occurred. Please try again');
+        }
+
+        console.log('File has been decompressed!');
+    });
 };
+
+compress();
--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,3 +1,21 @@
+import { createGunzip } from 'node:zlib';
+import { pipeline } from 'node:stream';
+import { createReadStream, createWriteStream } from 'node:fs';
+
 export const decompress = async () => {
-    // Write your code here 
+    const unzip = createGunzip();
+
+    const source = createReadStream('./files/archive.gz');
+    const destination = createWriteStream('./files/fileToCompress.txt');
+
+    pipeline(source, unzip, destination, (err) => {
+        if (err) {
+            console.log(err);
+            throw new Error('Error occurred. Please try again');
+        }
+
+        console.log('File has been compressed!');
+    });
 };
+
+decompress();
