309d25211eeafa58ef7228cc93cfb10a17ba4381
Aleksey Krupsky <aleksey.krupsky@wrenkitchens.com>
2022-06-03 13:04:50 +0300

    WEEK 1. CP

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,3 +1,11 @@
+import { argv } from 'node:process';
+import { spawn } from 'node:child_process';
+
 export const spawnChildProcess = async (args) => {
-    // Write your code here
+    args = args.slice(2);
+    args.unshift('./files/script.js');
+
+    spawn('node', args, { stdio: 'inherit'});
 };
+
+spawnChildProcess(argv);
