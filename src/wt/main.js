6d11e64892e6508e10f92aebc241c780648f4f0a
Aleksey Krupsky <aleksey.krupsky@wrenkitchens.com>
2022-06-03 08:53:54 +0300

    WEEK 1. Worker Threads

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,3 +1,34 @@
+import { Worker } from 'node:worker_threads';
+import os from 'node:os';
+
 export const performCalculations = async () => {
-    // Write your code here
+    const numOfCPUs = os.cpus().length;
+    const firstFibonacciNumber = 10;
+    const computationResult = [];
+
+    for (let i = 0; i < numOfCPUs; i++) {
+        const worker = new Worker('./worker.js', { workerData: firstFibonacciNumber + i });
+
+        worker.on('message', (result) => {
+            computationResult[i] = {
+                status: 'resolved',
+                data: result,
+            };
+        });
+
+        worker.on('error', () => {
+            computationResult[i] = {
+                status: 'error',
+                data: null,
+            };
+        });
+
+        worker.on('exit', (code) => {
+            if (Object.keys(computationResult).length === numOfCPUs) {
+                console.log(computationResult);
+            }
+        });
+    }
 };
+
+performCalculations();
